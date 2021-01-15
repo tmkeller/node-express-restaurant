faea8f3c346471467826f09859ac63306f422cc9
Tim Keller <timothy.m.keller@gmail.com>
2021-01-15 01:46:03 -0800

    Data routes completed.

--- index.js
+++ index.js
@@ -5,10 +5,61 @@ var PORT = process.env.PORT || 8080;
 app.use( express.urlencoded( { extended: true } ));
 app.use( express.json() );
 
+const tables = [
+    {
+        name: "John",
+        id: 1,
+        email: "john@john.com",
+        phone: 1234
+    }
+];
+
+const waitlist = [
+    {
+        name: "Jeff",
+        id: 2,
+        email: "no@table.sadface",
+        phone: 2345
+    }
+]
+
 app.use( express.static( 'public' ));
 
 app.get( '/', (req, res) => {
     res.send( 'Hello world!' );
+})
+
+app.get( '/reserve', (req, res) => {
+    res.send( 'Reserve page' );
+})
+
+app.get( '/api/tables', (req, res) => {
+    res.json( tables );
+})
+
+app.get( '/api/waitlist', (req, res) => {
+    res.json( waitlist );
+})
+
+app.post( '/reserve', ( req, res ) => {
+    console.log( req.body );
+    if ( tables.length < 5 ) {
+        tables.push( req.body );
+        console.log( `Tables: `, tables );
+        const responseObj = {
+            hasTable: true,
+            userData: req.body
+        }
+        res.json( responseObj );
+    } else {
+        waitlist.push( req.body );
+        const responseObj = {
+            hasTable: false,
+            userData: req.body
+        }
+        res.json( responseObj );
+        console.log( `Waitlist: `, waitlist )
+    }
 })
 
 app.listen( PORT, function() {
